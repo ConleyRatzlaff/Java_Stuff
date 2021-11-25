82162b430286e7ef322b90ac611942120bb0e7fb
ConleyRatzlaff <conleyratzlaff@gmail.com>
2021-11-24 20:15:25 -0500

    added Main.class and Main.java

--- project1/Main.java
+++ project1/Main.java
@@ -8,19 +8,19 @@ import java.util.Arrays;
 
 public class Main{
   int[] number = new int[20];
-  static void getRandom(){
+  public void getRandom(){
   //sets length of number array to 20
-  Main myObj = new Main();
+
   //generates 20 random numbers and stores them into number array
   for(int i = 0; i < number.length; i++){
-    myObj.number[i] = (int)(Math.random()*30 + 1);
+    number[i] = (int)(Math.random()*30 + 1);
 
   }
   //prints the contents of array
-  //System.out.println(Arrays.toString(number));
+  System.out.println(Arrays.toString(number));
   }
 
-  static void checkArray(){
+  /*static void checkArray(){
     int x = number[]
     for (int i= 0; i < number.length; i++){
       if ( x == number[i]){
@@ -29,13 +29,15 @@ public class Main{
       }else{
         return x;
     }
+
+
   }
 }
-
+*/
 
   public static void main(String[] args) {
   getRandom();
-  checkArray();
+  //checkArray();
   System.out.println(Arrays.toString(number));
   }
 }
